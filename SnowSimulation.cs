3ba59745998d3429c21a5fe3023dab88abde0619
Nikgor2002 <ngniknik@mail.ru>
2023-04-12 00:02:18 +0300

    Update NewPhysicisSNowFlakePro2
    Добавили движение по винтовой линии, что обычному человеческому глазу кажется более реалистичнее

--- SnowSimulation.cs
+++ SnowSimulation.cs
@@ -1,50 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class SnowSimulation : MonoBehaviour
+public class Snowflake : MonoBehaviour
 {
     public Material snowMaterial;
     public float particleSize = 0.1f;
-    public float windStrength = 0.1f;
 
     private ParticleSystem particleSystem;
     private ParticleSystem.MainModule mainModule;
     private ParticleSystem.EmissionModule emissionModule;
     private ParticleSystemRenderer particleSystemRenderer;
+    private float _tick = 0;
+    private Vector3 _startLocation;
 
     void Start()
     {
+        _startLocation = transform.localPosition;
         particleSystem = GetComponent<ParticleSystem>();
         mainModule = particleSystem.main;
         emissionModule = particleSystem.emission;
         particleSystemRenderer = GetComponent<ParticleSystemRenderer>();
 
-        mainModule.startLifetime = new ParticleSystem.MinMaxCurve(10f, 20f);
-        mainModule.startSpeed = new ParticleSystem.MinMaxCurve(0.1f, 0.5f);
-        mainModule.startSize = new ParticleSystem.MinMaxCurve(particleSize, particleSize * 2f);
+        mainModule.startLifetime = new ParticleSystem.MinMaxCurve(25f, 30f);
+        mainModule.startSpeed = new ParticleSystem.MinMaxCurve(0.2f, 0.3f);
+        mainModule.startSize = new ParticleSystem.MinMaxCurve(0.1f, 0.2f);
         mainModule.maxParticles = 1000000000;
         mainModule.loop = true;
+
         mainModule.simulationSpace = ParticleSystemSimulationSpace.World;
         mainModule.scalingMode = ParticleSystemScalingMode.Shape;
 
-        emissionModule.rateOverTime = 100f;
+        emissionModule.rateOverTime = 10000f;
 
-        particleSystemRenderer.material = snowMaterial;
-
-        ParticleSystem.VelocityOverLifetimeModule velocityOverLifetime = particleSystem.velocityOverLifetime;
-        velocityOverLifetime.x = new ParticleSystem.MinMaxCurve(-windStrength, windStrength);
-        velocityOverLifetime.y = new ParticleSystem.MinMaxCurve(-windStrength, windStrength);
-        velocityOverLifetime.z = new ParticleSystem.MinMaxCurve(-windStrength, windStrength);
     }
 
     void Update()
     {
-        mainModule.startSize = new ParticleSystem.MinMaxCurve(particleSize, particleSize * 2f);
-        ParticleSystem.VelocityOverLifetimeModule velocityOverLifetime = particleSystem.velocityOverLifetime;
-        velocityOverLifetime.x = new ParticleSystem.MinMaxCurve(-windStrength, windStrength);
-        velocityOverLifetime.y = new ParticleSystem.MinMaxCurve(-windStrength, windStrength);
-        velocityOverLifetime.z = new ParticleSystem.MinMaxCurve(-windStrength, windStrength);
+        _tick += 0.002f;
+        float x = (float)(10 * Math.Sin(_tick));
+        float y = _tick / 10;
+        float z = (float)(10 * Math.Cos(_tick));
+        Vector3 addVector = new Vector3(x, y, z);
+
+        Vector3 alfa = _startLocation - addVector;
+        if (alfa[1] < -4.0f)
+        {
+            alfa = _startLocation;
+            _tick = 0f;
+        }
+        transform.localPosition = alfa;
+
     }
 }
-
